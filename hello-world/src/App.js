00df54b370e2e2ffdfeefb0d25c6e4a1f49be963
quaziyadgar <quaziyadgar@github.com>
2022-02-15 18:50:15 +0530

    c

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Greet from './component/greet';
+import Welcome from './component/welcome';
+import wish from './component/hello2';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Greet/>
+        <Welcome/>
+        <wish/>
     </div>
   );
 }
---
+++ hello-world/src/component/hello.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+function hello(props) {
+    return (
+         <h1>Hello {props.name}</h1>
+    )
+}
+export default hello;
